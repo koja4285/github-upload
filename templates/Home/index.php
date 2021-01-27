b0a2bbc6f4afeb82b39e11daddc9a6a1bcd6c09c
koja4285 <koja@knights.ucf.edu>
2021-01-27 12:49:22 -0500

    change captions of redio buttons by the picture

--- templates/Home/index.php
+++ templates/Home/index.php
@@ -101,17 +101,17 @@ echo $this->Html->script('home', ['block' => 'script']);
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" 
                 onchange="changePhoto('formal')" checked>
-                <label class="form-check-label" for="flexRadioDefault1"> Formal </label>
+                <label class="form-check-label" for="flexRadioDefault1"> When I'm playing poker </label>
             </div>
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" 
                 onchange="changePhoto('smug3')">
-                <label class="form-check-label" for="flexRadioDefault2"> Smug 1 </label>
+                <label class="form-check-label" for="flexRadioDefault2"> When I get you</label>
             </div>
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" 
                 onchange="changePhoto('smug2')">
-                <label class="form-check-label" for="flexRadioDefault3"> Smug 2 </label>
+                <label class="form-check-label" for="flexRadioDefault3"> When I'm home alone </label>
             </div>
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4" 
