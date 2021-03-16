e262dd38e96a1b808f9cbc8baa012c426ae075e4
koja4285 <koja@knights.ucf.edu>
2021-03-16 14:36:20 -0400

    add bootstrap-icon

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -32,6 +32,8 @@ $currentController = $this->request->getParam('controller');
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 
+    <!-- bootstrap5 icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
 
 
     <?= $this->Html->css('bootstrap/bootstrap.min') ?>     <!-- bootstrap  -->
